220822e6e408cb10a3499bd47ed5cb8d1315a976
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-03-25 11:28:33 +0800

    addCodeFromMaterialModule

--- src/PersonDemo.java
+++ src/PersonDemo.java
@@ -1,5 +1,24 @@
 public class PersonDemo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Create a person using the default constructor
+        Person person1 = new Person();
+        // Update attributes using setter methods
+        person1.setName("Alice");
+        person1.setAge(25);
+        person1.setGender("Female");
+        
+        // Create a person using the parameterized constructor
+        Person person2 = new Person("Bob", 30, "Male");
+        
+        // Display information for person1
+        System.out.println("Person 1 Information:");
+        person1.displayInfo();
+        System.out.println(person1.greet());
+        System.out.println();
+        
+        // Display information for person2
+        System.out.println("Person 2 Information:");
+        person2.displayInfo();
+        System.out.println(person2.greet());
     }
 }
